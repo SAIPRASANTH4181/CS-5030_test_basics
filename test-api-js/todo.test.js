86ffa6e735fe2d5deb5cce7c1e389b31ebbdecd6
prasanth4181 <saiprasanth.bodaballa@slu.edu>
2022-11-06 23:35:44 -0600

    added test case for delete todo

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -28,6 +28,13 @@ describe('todo test suite', () => {
         }
         expect(todo_service.add_todo(a).todo.length).toEqual(4);
     });
+
+    test("checking delete todo",()=>{
+        x=todo_service.get_todos().todo.length;
+        c=Math.floor(Math.random() * (x)) + 1;
+        todo_service.delete_todo(c);
+        expect(todo_service.get_todos().todo.find(x => x['id'] == c)).toBe(undefined);
+    });
     
 
     // Write all your test cases here that corresponds to software requirements
