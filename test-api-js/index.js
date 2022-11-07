2ab8dd6acb9b5eccb68e76059c6dca449e575798
prasanth4181 <saiprasanth.bodaballa@slu.edu>
2022-11-06 22:27:44 -0600

    implemented empty funcs in todo

--- test-api-js/index.js
+++ test-api-js/index.js
@@ -4,16 +4,33 @@ var app = express();
 app.use(express.json());
 var todoservice =require('./todo.service.js');
 
+let todo_service = new todoservice();
 
 app.get('/todolist', function (req, res) {
   // console.log('API called with GET:' + req.url);
-  let todo_service = new todoservice();
   res.json(todo_service.get_todos());
 })
 
-
-
 // Add more api calls here for each requirement
+
+app.get('/addItem', function (req, res) {
+  let a = todo_service.get_todos();
+  let l = a['todo'].length;
+  let e ={
+    "title": 'T'+(l+1), "description": 'D'+(l+1),"done":false
+  }
+  // console.log('API called with GET:' + req.url);
+  res.json(todo_service.add_todo(e));
+})
+
+app.get('/deleteItem', function (req, res) {
+  // let a = todo_service.delete_todo();
+  let e ={
+    "title": 'T'+(l+1), "description": 'D'+(l+1),"done":false
+  }
+  // console.log('API called with GET:' + req.url);
+  // res.json(todo_service.add_todo(e));
+})
 
 // app.listen(3000, function () {
 //   // console.log(' app listening on port 3000!')
--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -1,14 +1,17 @@
 class todoservice{
     todo_data = {
         "todo":[{
+            "id":1,
             "title": "T1",
             "description": "D1",
             "done": false
         },{
+            "id":2,
             "title": "T1",
             "description": "D1",
             "done": false
         },{
+            "id":3,
             "title": "T1",
             "description": "D1",
             "done": false
@@ -19,19 +22,39 @@ class todoservice{
     }
 
     get_todos(){
+        this.todos=this.todo_data;
         return this.todos;
     }
 
     add_todo(todo){
         // Your code here
+        this.todo_data['todo'].push(todo)
+        // this.todo_data['todo']
+        this.todos=this.todo_data;
+        return this.todos;
     }
+    
 
     delete_todo(id){
         // Your code here
-    }
+        if(id>-1)
+        {
+            // this.todo_data['todo'].slice(id,1);
+         this.todo_data['todo'] = this.todo_data['todo'].filter(x => x['id'] != id);
+            this.todos=this.todo_data;
+        }
+        return this.todos;
 
+    }
+    
     update_todo(id, todo){
         // Your code here
+        if(id > 0)
+        {
+            this.todo_data['todo'][id-1] = todo;
+            this.todos=this.todo_data;
+        }
+        return this.todos; 
     }
 }
 
